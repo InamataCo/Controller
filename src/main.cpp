a4ae679644bdd7fe00cadc1e09f8ab2fd3b4c16a
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-08-12 01:24:34 +0200

    Restructure main event loop to a timer-based setup
    - The period of each task is defined (in the setup or later)
    - The periods of these tasks are independent from one another

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,87 @@
 #include "wifi.h"
 
 WiFiClient wifiClient;
+Timer timer;
 
 bernd_box::Io io;
 bernd_box::Mqtt mqtt(wifiClient, bernd_box::client_id, bernd_box::mqtt_server);
 bernd_box::Wifi wifi(bernd_box::ssid, bernd_box::password);
+
+int8_t checkConnectivityId;
+void checkConnectivity();
+
+int8_t readAnalogSensorsId;
+void readAnalogSensors();
+
+int8_t updateAciditySensorId;
+void updateAciditySensor();
+
+// If not connected to WiFi, attempt to reconnect. Finally reboot
+void checkConnectivity() {
+  io.setStatusLed(true);
+
+  if (!wifi.isConnected()) {
+    Serial.println("WiFi: Disconnected. Attempting to reconnect");
+    if (wifi.connect(bernd_box::wifi_connect_timeout) == false) {
+      Serial.printf("WiFi: Could not connect to %s. Restarting\n",
+                    bernd_box::ssid);
+      ESP.restart();
+    }
+  }
+
+  // If not connected to an MQTT broker, attempt to reconnect. Else reboot
+  if (!mqtt.isConnected()) {
+    Serial.println("MQTT: Disconnected. Attempting to reconnect");
+    if (mqtt.connect(bernd_box::connection_attempts) == false) {
+      Serial.println("MQTT: Could not connect to broker. Restarting\n");
+      ESP.restart();
+    }
+  }
+
+  io.setStatusLed(false);
+}
+
+// Read and then print the analog sensors
+void readAnalogSensors() {
+  io.setStatusLed(true);
+
+  // Start a measurement series for the acidity sensor
+  updateAciditySensorId = timer.every(30, updateAciditySensor);
+
+  // Read all sensors. Then print and send them over MQTT
+  Serial.printf("\n%-10s|%-4s|%-15s|%s\n", "Sensor", "Pin", "Value", "Unit");
+  Serial.printf("----------|----|---------------|----\n");
+  for (auto& it : io.adcs_) {
+    float value = io.readAnalog(it.first);
+
+    Serial.printf("%-10s|%-4i|%-15f|%s\n", it.second.name.c_str(),
+                  it.second.pin_id, value, it.second.unit.c_str());
+    mqtt.send(it.second.name.c_str(), value);
+  }
+
+  io.setStatusLed(false);
+}
+
+void updateAciditySensor() {
+  io.setStatusLed(true);
+
+  io.takeAcidityMeasurement();
+
+  // Once enough measurements have been taken, stop the task and send
+  if (io.isAcidityMeasurementFull()) {
+    timer.stop(updateAciditySensorId);
+
+    float measurement = io.getMedianAcidityMeasurement();
+    const bernd_box::AdcSensor& sensor =
+        io.adcs_.find(bernd_box::Sensor::kAciditiy)->second;
+
+    Serial.printf("Median acidity value is %f %s\n", measurement,
+                  sensor.unit.c_str());
+    mqtt.send(sensor.name, measurement);
+  }
+
+  io.setStatusLed(false);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -43,44 +120,9 @@ void setup() {
     Serial.println("IO: Initialization failed. Restarting\n");
     ESP.restart();
   }
+
+  checkConnectivityId = timer.every(1000, checkConnectivity);
+  readAnalogSensorsId = timer.every(1000, readAnalogSensors);
 }
 
-void loop() {
-  // Turn the blue on-board LED on during the loop
-  io.setStatusLed(true);
-
-  // If not connected to WiFi, attempt to reconnect. Finally reboot
-  if (!wifi.isConnected()) {
-    Serial.println("WiFi: Disconnected. Attempting to reconnect");
-    if (wifi.connect(bernd_box::wifi_connect_timeout) == false) {
-      Serial.printf("WiFi: Could not connect to %s. Restarting\n",
-                    bernd_box::ssid);
-      ESP.restart();
-    }
-  }
-
-  // If not connected to an MQTT broker, attempt to reconnect. Else reboot
-  if (!mqtt.isConnected()) {
-    Serial.println("MQTT: Disconnected. Attempting to reconnect");
-    if (mqtt.connect(bernd_box::connection_attempts) == false) {
-      Serial.println("MQTT: Could not connect to broker. Restarting\n");
-      ESP.restart();
-    }
-  }
-
-  // Read all sensors. Then print and send them over MQTT
-  Serial.printf("\n%-10s|%-4s|%-15s|%s\n", "Sensor", "Pin", "Value", "Unit");
-  Serial.printf("----------|----|---------------|----\n");
-  for (auto& it : io.adcs_) {
-    float value = io.readAnalog(it.first);
-
-    Serial.printf("%-10s|%-4i|%-15f|%s\n", it.second.name.c_str(),
-                  it.second.pin_id, value, it.second.unit.c_str());
-    mqtt.send(it.second.name.c_str(), value);
-    delay(1);
-  }
-
-  // Turn the blue on-board LED off before sleeping
-  io.setStatusLed(false);
-  delay(1000);
-}
+void loop() { timer.update(); }
